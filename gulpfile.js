cedfbb00692ffea800163a6fa4203272c0141224
jaasu <hernandezalan007@gmail.com>
2024-08-13 20:09:55 -0600

    diseno base de una pagina web para festival de musica

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+import {src, dest, watch} from 'gulp'
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+
+const sass = gulpSass(dartSass);
+
+export function css(done){
+
+    src('src/scss/app.scss', {sourcemaps: true})
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('build/css', {sourcemaps: true}))
+
+    done();
+}
+
+export function dev(){
+    watch('src/scss/**/*.scss', css)
+}
+
