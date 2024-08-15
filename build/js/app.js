fc2fa4db59312b7d59c45263d98bde139f225fac
jaasu <hernandezalan007@gmail.com>
2024-08-15 13:11:49 -0600

    se termina la galería con la función de abrir imágenes y cerrarlas.

--- build/js/app.js
+++ build/js/app.js
@@ -26,22 +26,41 @@ function crearGaleria(){
 }
 
 function mostarImagen(i){
+
+    const imagen = document.createElement('IMG');
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = 'imagen galería';
+
     //Generar modal
 
     const modal = document.createElement('DIV');
     modal.classList.add('modal');
-
     modal.onclick = cerrarModal;
+    modal.appendChild(imagen);
+
+    //Botón cerrar modal
+
+    const cerrarModalBtn = document.createElement('BUTTON');
+    cerrarModalBtn.textContent = 'X';
+    cerrarModalBtn.classList.add('btn-cerrar');
+    cerrarModalBtn.onclick = cerrarModal;
+    modal.appendChild(cerrarModalBtn);
 
     //Agregar al HTML
 
     const body = document.querySelector('body');
+    body.classList.add('overflow-hidden');
     body.appendChild(modal);
 
 }
 
 function cerrarModal(){
     const modal = document.querySelector('.modal');
+    modal.classList.add('fade-out')
+    setTimeout(() => {
+        modal?.remove();
 
-    modal?.remove();
+        const body = document.querySelector('body');
+        body.classList.remove('overflow-hidden');
+    }, 500);
 }
