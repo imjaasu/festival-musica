3d61eff70a10e58d2d296ef0dbd79fb200265f3c
jaasu <hernandezalan007@gmail.com>
2024-08-16 13:15:06 -0600

    se modificó el gulpfile para crear imagenes en formato webp y avif

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import fs from 'fs';
+import {glob} from 'glob';
 import {src, dest, watch, series} from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
@@ -28,6 +29,7 @@ export function css(done){
     done();
 }
 
+// Disminuye la resolución de imagenes usadas en la página
 export async function crop(done) {
     const inputFolder = 'src/img/gallery/full'
     const outputFolder = 'src/img/gallery/thumb';
@@ -56,10 +58,41 @@ export async function crop(done) {
     }
 }
 
-export function dev(){
-    watch('src/scss/**/*.scss', css)
-    watch('src/js/**/*.js', js)
+// Convierte imagenes a formato webp
+export async function imagenes(done) {
+    const srcDir = './src/img';
+    const buildDir = './build/img';
+    const images =  await glob('./src/img/**/*{jpg,png}')
+
+    images.forEach(file => {
+        const relativePath = path.relative(srcDir, path.dirname(file));
+        const outputSubDir = path.join(buildDir, relativePath);
+        procesarImagenes(file, outputSubDir);
+    });
+    done();
 }
 
-export default series(crop, js, css, dev)
+function procesarImagenes(file, outputSubDir) {
+    if (!fs.existsSync(outputSubDir)) {
+        fs.mkdirSync(outputSubDir, { recursive: true })
+    }
+    const baseName = path.basename(file, path.extname(file))
+    const extName = path.extname(file)
+    const outputFile = path.join(outputSubDir, `${baseName}${extName}`)
+    const outputFileWebp = path.join(outputSubDir, `${baseName}.webp`)
+    const outputFileAvif = path.join(outputSubDir, `${baseName}.avif`)
+
+    const options = { quality: 80 }
+    sharp(file).jpeg(options).toFile(outputFile)
+    sharp(file).webp(options).toFile(outputFileWebp)
+    sharp(file).avif().toFile(outputFileAvif)
+}
+
+export function dev(){
+    watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', js);
+    watch('src/img/**/*.{png,jpg}', imagenes);
+}
+
+export default series(imagenes, crop, js, css, dev);
 
