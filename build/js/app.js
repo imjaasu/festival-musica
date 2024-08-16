d93ff09057725a654272341b22d686cf47bf1333
jaasu <hernandezalan007@gmail.com>
2024-08-15 21:29:46 -0600

    se agregó la función para resaltar la sección en el header que esté en pantalla

--- build/js/app.js
+++ build/js/app.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
     crearGaleria();
-
     navegacionFija();
-
+    resaltarEnlace();
+    scrollNav();
+    
 })
 
 function navegacionFija(){
@@ -79,4 +80,31 @@ function cerrarModal(){
         const body = document.querySelector('body');
         body.classList.remove('overflow-hidden');
     }, 500);
+}
+
+function resaltarEnlace(){
+    document.addEventListener('scroll', function(){
+        const sections = document.querySelectorAll('section');
+        const navLinks = document.querySelectorAll('.navegacion-principal a');
+
+        let actual = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (window.scrollY >= (sectionTop - sectionHeight / 3)){
+                actual = section.id;
+            }
+        })
+
+        navLinks.forEach(link => {
+
+            link.classList.remove('active');
+
+            if(link.getAttribute('href') === '#' + actual){
+                link.classList.add('active');
+            }
+        })
+    })
 }
